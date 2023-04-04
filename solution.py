0861102fb7e9a97c0b03892d04c325e81e1c7738
ReginaMiKi <129621290+ReginaMiKi@users.noreply.github.com>
2023-04-04 21:27:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 730773348 t ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return np.sum(x)/(n*t)
