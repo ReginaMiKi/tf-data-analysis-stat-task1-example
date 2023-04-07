406e4f69b4c57b148ac7d89a9efd113a4461c3ef
ReginaMiKi <129621290+ReginaMiKi@users.noreply.github.com>
2023-04-07 19:12:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,5 +8,5 @@ def solution(x: np.ndarray) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    t = 34
-    return np.mean(x)/(n*t)
+    t=34
+    return np.mean(x)/np.sum(x)*t*n
