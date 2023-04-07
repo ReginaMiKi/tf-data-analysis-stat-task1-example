725fd7164d34c7d9bf24c910959e63ca0aec5ae7
ReginaMiKi <129621290+ReginaMiKi@users.noreply.github.com>
2023-04-07 18:25:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ import numpy as np
 
 chat_id = 730773348 t ID, не меняйте название переменной
 
-def solution(x: np.array) -> float:
+def solution(x: np.ndarray) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
